112d619de9be5491c2e92466d193e8c316a4f612
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2026-06-11 20:58:13 -0300

    Ajustes em geral

---
+++ Banco.Aplicacao/DTO/AppSettingsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Banco.Aplicacao.DTO
+{
+    public class AppSettingsDto
+    {
+ 
+        public ConnectionStringsDto ConnectionStrings { get; set; } = new ConnectionStringsDto();
+ 
+    } 
+
+ 
+    public class ConnectionStringsDto
+    {
+        public string ConexaoServidor { get; set; } = string.Empty;
+        public string ConexaoServidorQuery { get; set; } = string.Empty;
+    }
+
+ 
+}
